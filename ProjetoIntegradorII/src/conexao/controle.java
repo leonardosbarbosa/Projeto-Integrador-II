59be961971674ab0f6935364f524784948e496b0
Lukas Matias <lukaskt6@gmail.com>
2019-10-03 01:35:56 -0300

    kk

--- ProjetoIntegradorII/src/conexao/controle.java
+++ ProjetoIntegradorII/src/conexao/controle.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conexao;
 
 import Modelos.ModeloProduto;
@@ -10,10 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author lukas
- */
 public class controle {
 
     conexaoBD conex = new conexaoBD();
@@ -22,8 +14,8 @@ public class controle {
     public void salvar(ModeloProduto mod) throws SQLException {
         try {
             conex.conexao(); // conecta no banco de dados
-            PreparedStatement pst = conex.con.prepareStatement("insert into produto values (?,?,?,?,?)");
-            pst.setInt(1, mod.getCodProduto());
+            PreparedStatement pst = conex.con.prepareStatement("insert into produto values (?,?,?,?,?)"); // comando para inserir dados na tabela
+            pst.setInt(1, mod.getCodProduto()); // passando a posição da referencia do argumento em cada item
             pst.setString(2, mod.getProduto());
             pst.setString(3, mod.getDescProduto());
             pst.setInt(4, mod.getQuantProduto());
--- ProjetoIntegradorII/src/projetointegradorii/FrInicio.java
+++ ProjetoIntegradorII/src/projetointegradorii/FrInicio.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projetointegradorii;
 import  projetointegradorii.Produtos;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Leonardo Silva
- */
 public class FrInicio extends javax.swing.JFrame {
 
     public FrInicio() throws SQLException {Produtos produto = new Produtos();
@@ -145,34 +137,8 @@ public class FrInicio extends javax.swing.JFrame {
         produto.setVisible(true);
     }//GEN-LAST:event_btnProdutosActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+      
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
